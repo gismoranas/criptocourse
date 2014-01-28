9d163675e03fdf448bce0d0df0394bba966c1d63
Gismo Ranas <cataldomanigrasso@gmail.com>
2014-01-28 13:55:30 +0100

    changes to main file, working.

--- week1-extra.py
+++ week1-extra.py
@@ -8,6 +8,11 @@ import sys
 from collections import Counter, defaultdict
 from operator import itemgetter
 
+from collection_utils import find_most_common_entries
+
+#TODO move msg to other file and read it, so it can be accidentally modified
+#TODO make some order
+
 msg = ['315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e', 
   '234c02ecbbfbafa3ed18510abd11fa724fcda2018a1a8342cf064bbde548b12b07df44ba7191d9606ef4081ffde5ad46a5069d9f7f543bedb9c861bf29c7e205132eda9382b0bc2c5c4b45f919cf3a9f1cb74151f6d551f4480c82b2cb24cc5b028aa76eb7b4ab24171ab3cdadb8356f',
   '32510ba9a7b2bba9b8005d43a304b5714cc0bb0c8a34884dd91304b8ad40b62b07df44ba6e9d8a2368e51d04e0e7b207b70b9b8261112bacb6c866a232dfe257527dc29398f5f3251a0d47e503c66e935de81230b59b7afb5f41afa8d661cb',
@@ -102,6 +107,12 @@ def print_xor_ascii():
     string = hex_string.decode('hex')
     print hex_string, string, xor_hexed(hex_string, '20')
 
+#def find_most_common_entries(some_collection):
+#  '''Returns a list with the entires with maximum cardinality.'''
+#  counter = Counter(some_collection).most_common()
+#  max_cardinality = counter[0][1]
+#  return [ a[0] for a in counter if a[1] == max_cardinality ]
+
 def easy_decrypt(chosen_index):
   cache = []
   chosen_msg = msg[chosen_index]
@@ -115,14 +126,18 @@ def easy_decrypt(chosen_index):
     for key in cache_element:
       index_to_letters[key].append(cache_element[key])
   plaintext = ['*']*(len(chosen_msg)/2)
-  for index in index_to_letters:
-    counter = Counter(index_to_letters[index]).most_common()
-    max_length = max(counter, key = itemgetter(1))[1]
-    if max_length > 2:
-      most_common = [ a[0] for a in counter if a[1] == max_length ]
-      #print index, most_common, max_length
-      if len(most_common) == 1:
-        plaintext[index] = most_common[0]
+  for index, letters in index_to_letters.iteritems():    
+    most_common = find_most_common_entries(letters)
+    if most_common[1] > 2 and len(most_common[0]) == 1:
+        plaintext[index] = most_common[0][0]
+
+#    counter = Counter(letters).most_common()
+#    max_length = max(counter, key = itemgetter(1))[1]
+#    if max_length > 2:
+#      most_common = [ a[0] for a in counter if a[1] == max_length ]
+#      #print index, most_common, max_length
+#      if len(most_common) == 1:
+#        plaintext[index] = most_common[0]
   plaintext = ''.join(plaintext)
   key = str_xor(plaintext, chosen_msg.decode('hex')).encode('hex')
   print plaintext
@@ -145,8 +160,8 @@ def main():
         index_to_hex[j].append(split_key[j])
   for index in index_to_hex:
     print index, index_to_hex[index]
+  
 
-    
   #decrypt()
   #key = str_xor('The sXXue', msg[10].decode('hex')).encode('hex')
   #for i in range(0,11):
