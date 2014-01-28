cf29abc8c9a4935d3731e6460ca3f4f57001d6e8
Gismo Ranas <cataldomanigrasso@gmail.com>
2014-01-28 19:24:19 +0100

    finished refactoring, working bad as before

--- week1-extra.py
+++ week1-extra.py
@@ -31,59 +31,64 @@ def xor_hexes_and_find_ascii_chars(index, cyphertexts):
     index_to_ascii_char_list.append(find_ascii_chars_in_hex(xored_hexes))
   return index_to_ascii_char_list
 
-def easy_decrypt(index, cyphertexts):
-  index_to_ascii_char_list = xor_hexes_and_find_ascii_chars(index, cyphertexts)
-      
-  cyphertext = cyphertexts[index]
+def find_most_probable_plaintext(index_to_ascii_char_list, plaintext_size):
   index_to_letters = defaultdict(list)
-  for cache_element in index_to_ascii_char_list:
-    for key in cache_element:
-      index_to_letters[key].append(cache_element[key])
-  plaintext = ['*']*(len(cyphertext)/2)
-  for i, letters in index_to_letters.iteritems():    
+  for index_to_ascii_char in index_to_ascii_char_list:
+    for index_1, ascii_char in index_to_ascii_char.iteritems():
+      index_to_letters[index_1].append(ascii_char)
+  plaintext = ['*']*plaintext_size
+  for index_1, letters in index_to_letters.iteritems():    
     most_common = find_most_common_entries(letters)
     if most_common[1] > 2 and len(most_common[0]) == 1:
-        plaintext[i] = most_common[0][0]
+        plaintext[index_1] = most_common[0][0]
+  return ''.join(plaintext)
 
-  plaintext = ''.join(plaintext)
-  key = xor_string_hex(plaintext, cyphertext).encode('hex')
-  key_as_string = ''
+def generate_key_from_noisy_plaintext_and_cyphertext(plaintext, cyphertext):
+  key_with_noise = xor_string_hex(plaintext, cyphertext).encode('hex')
+  key = ''
   for i in range(len(plaintext)):
     if plaintext[i] == '*':
-      key_as_string += '*'
+      key += '*'
     else:
-      key_as_string += key[i*2:i*2+2]
-  return key_as_string
+      key += key_with_noise[i*2:i*2+2]
+  return key
 
-def merge_keys(index_to_hexes):
-  key = ['00']*(max(index_to_hexes.keys()) + 1)
-  for index, hexes in index_to_hexes.iteritems():
+def generate_noisy_key(index, cyphertexts):
+  index_to_ascii_char_list = xor_hexes_and_find_ascii_chars(index, cyphertexts)
+  plaintext = find_most_probable_plaintext(index_to_ascii_char_list, len(cyphertexts[index])/2)
+  return generate_key_from_noisy_plaintext_and_cyphertext(plaintext, cyphertexts[index])
+
+def find_most_probable_key(index_to_key_hexes):
+  key = ['00']*(max(index_to_key_hexes.keys()) + 1)
+  for index, hexes in index_to_key_hexes.iteritems():
     most_common = find_most_common_entries(hexes)
     if len(most_common[0]) > 1: 
       key[index] = '?'
     else:
       key[index] = most_common[0][0]  
   return ''.join(key)
-  
 
-def main():
+def merge_noisy_keys(noisy_keys):
   keys = []
-  index_to_hexes = defaultdict(list)
-  cyphertexts = read_cyphertexts()
-  for c in cyphertexts:
-    print c
-  for i in range(11):
-    key = easy_decrypt(i, cyphertexts)
-    split_key = re.findall('\*|[0-9a-f]{2}', key)
+  index_to_key_hexes = defaultdict(list)
+  for i in range(len(noisy_keys)):
+    split_key = re.findall('\*|[0-9a-f]{2}', noisy_keys[i])
     for j in range(len(split_key)):
       if split_key[j] != '*':
-        index_to_hexes[j].append(split_key[j])
+        index_to_key_hexes[j].append(split_key[j])
+  return find_most_probable_key(index_to_key_hexes)
 
-  for index, hexes in index_to_hexes.iteritems():
-    print index, hexes
+def main():
+  cyphertexts = read_cyphertexts()
 
-  key = merge_keys(index_to_hexes)
+  noisy_keys = []
+  for i in range(len(cyphertexts)):
+    noisy_keys.append(generate_noisy_key(i, cyphertexts))
+
+  key = merge_noisy_keys(noisy_keys)
+
   print key
+
   for cyphertext in cyphertexts:
     print 'cyphertext', cyphertext[:10] 
     print 'xor', xor_hexes(cyphertext[:10], key)  
